e696b50e85cf15168cce1823c86b7c7c7af497b2
Carmeline Dsilva <cdsilva@matthew-remillards-macbook-pro.local>
2014-02-13 19:23:06 -0500

    Updated code to make figures

--- image_analysis_paper/make_figures_large_dataset.m
+++ image_analysis_paper/make_figures_large_dataset.m
@@ -20,7 +20,7 @@ mem_lengths = length;
 clear length;
 
 m = length(mem_lengths);
-m = 16;
+%m = 16;
 
 %% load images
 
@@ -32,22 +32,56 @@ subplot_dim2 = ceil(m / subplot_dim1);
 
 image_set = zeros(npixels, npixels, m);
 
-image_channel = 2;
+image_channel = 3;
 
 % image indices to plot
 % im_save_idx = [1,19,25,30,35,40,45,49];
 
+%figure;
+for i=1:m
+    % read image
+    im1 = imread(sprintf('%s/lat%02d.tif', dpERK_image_dir, i));
+
+    % resize image
+    im1 = imresize(im1, [npixels npixels]);
+
+    % extract relevent color from image
+    im1 = im1(:,:,image_channel);
+
+    %subplot(subplot_dim1, subplot_dim2, i)
+    %imshow(im1);
+    
+    %store image
+    im1 = double(im1);
+    image_set(:, :, i) = im1;
+    
+end
+
+%%
+% [~, ind] = max(mem_lengths);
+% 
+% im_template = imread(sprintf('%s/lat%02d.tif', dpERK_image_dir,ind));
+% %resize image
+% im_template = imresize(im_template, [npixels npixels]);
+% %extract relevent color from image
+% im_template = im_template(:,:,image_channel);
+% 
+% theta_vec = linspace(0, 360, 21);
+% theta_vec = theta_vec(1:end-1);
+% 
+% thetas = zeros(m,1);
+% xdisp = zeros(m,1);
+% ydisp = zeros(m,1);
+% 
 % figure;
 % for i=1:m
-%     % read image
-%     im1 = imread(sprintf('%s/lat%02d.tif', dpERK_image_dir, i));
-% 
-%     % resize image
-%     im1 = imresize(im1, [npixels npixels]);
-% 
-%     % extract relevent color from image
-%     im1 = im1(:,:,image_channel);
-% 
+%     thetas(i) = theta_vec(randi(20,1));
+%     xdisp(i) = randi([-10 10], 1);
+%     ydisp(i) = randi([-10 10], 1);
+%     
+%     im1 = imrotate(im_template, thetas(i), 'crop');
+%     im1 = circshift(im1, [xdisp(i) ydisp(i)]);
+%     
 %     subplot(subplot_dim1, subplot_dim2, i)
 %     imshow(im1);
 %     
@@ -56,61 +90,26 @@ image_channel = 2;
 %     image_set(:, :, i) = im1;
 %     
 % end
+% 
 
 %%
-[~, ind] = max(mem_lengths);
+m = 81;
+[~, ind] = sort(mem_lengths);
+ind = ind(end-m+1:end);
+ind = ind(randperm(m));
 
-im_template = imread(sprintf('%s/lat%02d.tif', dpERK_image_dir,ind));
-%resize image
-im_template = imresize(im_template, [npixels npixels]);
-%extract relevent color from image
-im_template = im_template(:,:,image_channel);
+mem_lengths = mem_lengths(ind);
+image_set = image_set(:,:,ind);
 
-theta_vec = linspace(0, 360, 21);
-theta_vec = theta_vec(1:end-1);
+%set image plotting parameters
+subplot_dim1 = ceil(sqrt(m));
+subplot_dim2 = ceil(m / subplot_dim1);
 
-thetas = zeros(m,1);
-xdisp = zeros(m,1);
-ydisp = zeros(m,1);
-
-figure;
-for i=1:m
-    thetas(i) = theta_vec(randi(20,1));
-    xdisp(i) = randi([-10 10], 1);
-    ydisp(i) = randi([-10 10], 1);
-    
-    im1 = imrotate(im_template, thetas(i), 'crop');
-    im1 = circshift(im1, [xdisp(i) ydisp(i)]);
-    
-    subplot(subplot_dim1, subplot_dim2, i)
-    imshow(im1);
-    
-    %store image
-    im1 = double(im1);
-    image_set(:, :, i) = im1;
-    
-end
-
-
-%%
-% [~, ind] = sort(mem_lengths);
-% ind = ind(end-10:end);
-% 
-% mem_lengths = mem_lengths(ind);
-% image_set = image_set(:,:,ind);
-% m = length(mem_lengths);
-% 
-% %set image plotting parameters
-% subplot_dim1 = ceil(sqrt(m));
-% subplot_dim2 = ceil(m / subplot_dim1);
-% 
 % figure;
 % for i=1:m
 %     subplot(subplot_dim1, subplot_dim2, i)
 %     imshow(uint8(image_set(:,:,i)));
 % end
-% 
-% L = mem_lengths;
 
 %% raw dpERK images-- synchronization
 
@@ -118,58 +117,56 @@ angle_proj = pi/8;
 shift_max = 20;
 shift_step = 4;
 dim = 3;
+
+tic
 [R, W] = compute_pairwise_alignments(image_set, angle_proj, shift_max, shift_step);
 
 %%
 
 eps = median(W(:));
-%neigs = 5;
-%[R_opt, embed_coord, embed_idx, D] = vdm(R, W, eps, neigs);
-%R_opt = ang_synch(R, 3);
+neigs = 5;
 
-[V, D] = eigs(R, dim);
-if det(V(1:3,:)*D*V(1:3,:)') < 0
-    V(:,end) = -V(:,end);
-end
+%R_opt = ang_synch(R, dim);
+[R_opt, embed_coord, embed_idx, D] = vdm(R, W, eps, neigs);
 
-%R_opt =  V*sqrt(D);
-% R_opt = zeros(size(V));
-% for i=1:m
-%     R_opt(dim*(i-1)+1:dim*i,:) = V(dim*(i-1)+1:dim*i,:) * inv(sqrtm(V(dim*(i-1)+1:dim*i,:)'*V(dim*(i-1)+1:dim*i,:)));
-%     det(R_opt(dim*(i-1)+1:dim*i,:))
-% end
-
-%R2 = V*D*V';
-
-R_opt = V*D*V(1:3,:)';
-% R_opt = zeros(size(V));
-% for i=1:m
-%     [u, s, v] = svd(V(dim*(i-1)+1:dim*i,:));
-%     R_opt(dim*(i-1)+1:dim*i,:) = u * v';
-% end
-% 
-% for i=1:m
-%     [u, s, v] = svd(V(dim*(i-1)+1:dim*i,:));
-%     R_opt(dim*(i-1)+1:dim*i,:) = R_opt(dim*(i-1)+1:dim*i,:) * R_opt(1:3,:);
-% end
-
-for i=1:m
-    R_opt(dim*(i-1)+1:dim*i,:) = R_opt(dim*(i-1)+1:dim*i,:) /(det(R_opt(dim*(i-1)+1:dim*i,:))^(1/3));
-end
+toc
 
 %%
 
 
+[~, idx] = max(abs(corr(embed_coord, mem_lengths)));
+idx = 7;
+
+if corr(mem_lengths, embed_coord(:, idx)) < 0
+    embed_coord(:, idx) = -embed_coord(:, idx);
+end
+
+figure; 
+plot(mem_lengths, embed_coord(:, idx),'.')
+xlabel('membrane length')
+ylabel(sprintf('$$ \\langle \\phi_%d, \\phi_%d \\rangle $$ (using DI data)', embed_idx(1, idx), embed_idx(2, idx)),'interpreter','latex')
+
+[~, I] = sort(embed_coord(:, idx));
 figure;
-j = m-1;
 for i=1:m
-    %im1 = rotate_image(R_opt(1:dim,1:dim)'*R_opt(dim*(i-1)+1:dim*i,:), uint8(image_set(:,:,i)), angle_proj);
-    im1 = rotate_image(R_opt(dim*(i-1)+1:dim*i,:), uint8(image_set(:,:,i)), angle_proj);
-    det(R_opt(dim*(i-1)+1:dim*i,:))
-    %det(R_opt(1:dim,1:dim)'*R_opt(dim*(i-1)+1:dim*i,:))
-    %im1 = rotate_image(R2(dim*(i-1)+1:dim*i,dim*(j-1)+1:dim*j), uint8(image_set(:,:,i)), angle_proj);
+    im1 = rotate_image(R_opt(dim*(I(i)-1)+1:dim*I(i),:), uint8(image_set(:,:,I(i))), angle_proj);
+    %det(R_opt(dim*(i-1)+1:dim*i,:))
     subplot(subplot_dim1, subplot_dim2, i);
     imshow(im1);
 end
 
+figure;
+for i=1:m
+    im1 = uint8(image_set(:,:,i));
+    subplot(subplot_dim1, subplot_dim2, i);
+    imshow(im1);
+end
 
+%%
+r1 = compute_ranks(mem_lengths);
+r2 = compute_ranks(embed_coord(:,idx));
+
+figure; 
+plot(r1, r2, '.')
+xlabel('rank from membrane lengths')
+ylabel('rank from dmaps using DI data')
