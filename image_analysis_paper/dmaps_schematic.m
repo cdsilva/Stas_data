09ee1829469978e52f1d6bc451eb3ffcb1d13513
Carmeline Dsilva <cdsilva@matthew-remillards-macbook-pro.local>
2014-03-25 22:20:12 -0400

    updated paper

--- image_analysis_paper/dmaps_schematic.m
+++ image_analysis_paper/dmaps_schematic.m
@@ -1,11 +1,16 @@
 clear all
 close all
 
-n = 20;
+res = '-r300';
+fmt = '-djpeg';
+
+im_save_dir = 'paper_figures';
+%%
+n = 15;
 theta = linspace(0, 3*pi/2, n);
 
-x = cos(theta);
-y = sin(theta);
+x = 0.9*cos(theta);
+y = 0.9*sin(theta);
 
 %x = -1:0.1:1;
 %y = x.^3;
@@ -20,21 +25,28 @@ eps = median(W(:))/2;
 A = exp(-W/eps);
 %A = diag(1./sum(A))*A;
 
-figure;
-plot(x, y, '.', 'markersize', 10)
 
+%%
 
 figure;
 for i=1:n
     for j=1:n-1
         idx = ceil(size(cm, 1) / max(A(:)) * A(i,j));
-        plot([x(i) x(j)], [y(i) y(j)], 'color', cm(idx, :), 'linewidth', 1);
+        plot([x(i) x(j)], [y(i) y(j)], 'color', cm(idx, :), 'linewidth', 2);
         hold on
     end
 end     
-plot(x, y, '.', 'markersize', 20)
+plot(x, y, '.', 'markersize', 50)
+set(gca, 'xtick', [])
+set(gca, 'ytick', [])
+%axis off
+print(sprintf('%s/dmaps_schematic_edges', im_save_dir), fmt, res);
+
 
 [V, D] = dmaps(W, eps, 2);
 figure;
-scatter(x, y, 500, V(:,2), '.')
+scatter(x, y, 2000, V(:,2), '.')
+set(gca, 'xtick', [])
+set(gca, 'ytick', [])
+print(sprintf('%s/dmaps_schematic_color', im_save_dir), fmt, res);
 
