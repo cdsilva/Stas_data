f338394506b8ccd5d24ac5aab7e6941d195de023
Carmeline Dsilva <cdsilva@matthew-remillards-macbook-pro.local>
2014-05-14 23:25:11 -0400

    added more tests for mutants

--- image_analysis_paper/test_mutants.m
+++ image_analysis_paper/test_mutants.m
@@ -4,8 +4,14 @@ close all
 %% directories where things are stored
 
 image_dir = '../membrane_pictures/snanull_big';
-
 m = 98;
+
+% image_dir = '../membrane_pictures/snanull_old';
+% m = 54;
+
+%image_dir = '../membrane_pictures/snanull_old2';
+%m = 47;
+
 ind = 1:m;
 
 %% image parameters
@@ -44,7 +50,7 @@ for i=1:m
     %store image
     image_set(:, :, :, i) = im1;
     
-        make_subplot(subplot_dim1, subplot_dim2, 0.01, i);
+    make_subplot(subplot_dim1, subplot_dim2, 0.01, i);
     imshow(im1);
 end
 
@@ -55,9 +61,9 @@ shift_max = 10;
 shift_step = 2;
 dim = 3;
 
-% matlabpool open 4;
-% [R, W] = compute_pairwise_alignments_color(image_set, angle_proj, shift_max, shift_step);
-% matlabpool close
+%matlabpool open 2;
+%[R, W] = compute_pairwise_alignments_color(image_set, angle_proj, shift_max, shift_step);
+%matlabpool close
 
 %save('pairwise_alignments_snanull.mat', 'R', 'W');
 load('pairwise_alignments_snanull.mat');
@@ -66,20 +72,95 @@ load('pairwise_alignments_snanull.mat');
 %% select which images to use
 
 % ind = setdiff(1:m, [61 93 39 9 76 89 40 63 83 58]);
-% 
+%
 % image_set = image_set(:, :, :, ind);
 % image_set_raw = image_set_raw(:, :, :, ind);
 % nuclei = nuclei(:, :, ind);
-% 
+%
 % W = W(ind, ind);
-% 
+%
 % R_ind = reshape(dim*repmat(ind, dim, 1) - repmat((dim-1:-1:0)', 1, length(ind)), [], 1)';
 % R = R(R_ind, R_ind);
-% 
+%
 % m = length(ind);
-% 
+%
 % subplot_dim1 = floor(sqrt(m));
 % subplot_dim2 = ceil(m / subplot_dim1);
+
+%% just dmaps
+
+% for i=1:m
+%     W(i, i) = 0;
+% end
+
+eps = median(W(:));
+
+[V, D] = dmaps(W, eps, 10);
+
+figure;
+plot(V(:,2),V(:,3),'.')
+
+
+%% synchronization
+
+R_opt = ang_synch(R, dim);
+
+image_set_aligned = zeros(size(image_set), 'uint8');
+image_set_aligned_withnuclei = zeros(size(image_set), 'uint8');
+for i=1:m
+    im_tmp = image_set(:,:,:,i);
+    %         for j=1:3
+    %             im_tmp(:,:,j) = im_tmp(:,:,j) + nuclei(:,:,i);
+    %         end
+    im1 = rotate_image(R_opt(dim*(i-1)+1:dim*i,:), im_tmp, angle_proj);
+    image_set_aligned(:,:,:,i) = im1;
+    
+    im_tmp = image_set(:,:,:,i);
+    for j=1:3
+        im_tmp(:,:,j) = im_tmp(:,:,j) + nuclei(:,:,i);
+    end
+    im1 = rotate_image(R_opt(dim*(i-1)+1:dim*i,:), im_tmp, angle_proj);
+    image_set_aligned_withnuclei(:,:,:,i) = im1;
+end
+
+%% dmaps after synch
+
+figure;
+for i=1:m
+    make_subplot(subplot_dim1, subplot_dim2, 0.01, i);
+    
+    imshow(image_set_aligned(:,:,:,i));
+end
+
+W2 = inf(m);
+
+for i=1:m
+    im1 = image_set_aligned(:,:,:,i);
+    for j=1:i-1
+        im2 = image_set_aligned(:,:,:,j);
+        W2(i, j) = sum((double(im1(:)) - double(im2(:))).^2);
+        W2(j, i) = W2(i, j);
+    end
+end
+
+eps2 = median(W2(:));
+
+[V2, D] = dmaps(W2, eps2, 10);
+
+figure;
+plot(V2(:,2),V2(:,3),'.')
+
+
+figure;
+im_delta = 0.02;
+plot(V2(:,2),V2(:,3),'.')
+hold on
+for i=1:m
+    image('cdata', image_set_aligned(:, :, :, i), 'xdata', [V2(i,2) V2(i,2)+im_delta], 'ydata', [V2(i,3) V2(i,3)+im_delta])
+    hold on
+end
+
+return
 
 %% VDM
 
@@ -147,13 +228,38 @@ for i=1:nstages
 end
 
 %%
-% W2 = squareform(pdist(embed_coord(:, 1:15))).^2;
-% eps2 = median(W2(:))*1000;
-% 
-% [V2, D2] = dmaps(W2, eps2, 10);
-% 
-% figure;
-% plot(V2(:,2),V2(:,3),'.')
+
+eps = median(W(:));
+[V2, D2] = dmaps(W, eps, 10);
+
+figure;
+plot(V2(:,2),V2(:,3),'.')
+
+figure;
+im_delta = 0.02;
+plot(V2(:,2),V2(:,3),'.')
+hold on
+for i=1:m
+    image('cdata', image_set_aligned_withnuclei(:, :, :, i), 'xdata', [V2(i,2) V2(i,2)+im_delta], 'ydata', [V2(i,3) V2(i,3)+im_delta])
+    hold on
+end
+
+
+%%
+
+embed_coord2 = embed_coord;
+for i=1:size(embed_coord, 2)
+    for j=1:2
+        embed_coord2(:, i) = embed_coord2(:, i) * sqrt(D(embed_idx(j,i), embed_idx(j,i)));
+    end
+end
+W2 = squareform(pdist(embed_coord2)).^2;
+eps2 = median(W2(:))*100;
+
+[V2, D2] = dmaps(W2, eps2, 10);
+
+figure;
+plot(V2(:,2),V2(:,3),'.')
 
 
 
