04aa53c869a74c64bb512be3f0186802fedbeb41
cdsilva <carmeline.dsilva@gmail.com>
2014-07-23 16:43:21 -0400

    added time warp code

--- live_imaging/DTW.m
+++ live_imaging/DTW.m
@@ -1,16 +1,48 @@
-function D = DTW(d)
+function [D, warp_path] = DTW(d)
+%% d is the pairwise distance matrix
+%% D is the time warp matrix (cumulative distance over paths)
+%% warp_path contains the optimal time warp path (warp_path(i,j) = 1 if points i and j are connected/linked)
 
 [m, n] = size(d);
 
 D = zeros(size(d));
 
-D(1, 2:end) = inf;
-D(2:end, 1) = inf;
+D(1,:) = cumsum(d(1,:));
 
-for i=2:n
-    for j=2:(m-i)
-        D(i,j) = d(i,j) + min([D(i-1, j) D(i-1,j-1) D(i,j-1)]);
+for i=2:m
+    j = 1;
+    D(i,j) = d(i,j) + D(i-1, j);
+    for j=2:n
+        D(i,j) = d(i,j) + min([D(i-1, j), D(i, j-1), D(i-1, j-1)]);
     end
 end
 
+%%
+warp_path = zeros(size(d));
 
+i = m;
+j = n;
+warp_path(i,j) = 1;
+
+while i > 1 || j > 1
+    if i > 1
+        i_tmp = i-1;
+        j_tmp = j;
+        if j > 1
+            if D(i-1,j-1) < D(i_tmp, j_tmp)
+                i_tmp = i-1;
+                j_tmp = j-1;
+            end
+            if D(i,j-1) < D(i_tmp, j_tmp)
+                i_tmp = i;
+                j_tmp = j-1;
+            end
+        end
+    else
+        i_tmp = i;
+        j_tmp = j-1;
+    end
+    i = i_tmp;
+    j = j_tmp;
+    warp_path(i,j) = 1;
+end
