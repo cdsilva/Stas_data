e5f13dae64c59c8f730c360926bce6f31eb50444
cdsilva <carmeline.dsilva@gmail.com>
2014-08-25 16:58:10 -0400

    added code for shuffled movie

---
+++ image_analysis_paper2/image_fn_shuffledmovie.m
@@ -0,0 +1,16 @@
+function image2 = image_fn_shuffledmovie(image, npixels)
+
+filt = fspecial('disk');
+
+image2 = imresize(image, [npixels npixels]);
+
+channel = 0;
+image2 = crop_image(image2, channel);
+
+image2 = padarray(image2, [10 10]);
+image2 = imresize(image2, [npixels npixels]);
+
+% image2 = imadjust(image2);
+image2 = adapthisteq(image2);
+
+image2 = imfilter(image2, filt, 'replicate');
---
+++ image_analysis_paper2/make_figures_shuffledmovie.m
@@ -0,0 +1,114 @@
+clear all
+close all
+
+%%
+
+npixels = 100;
+
+dt = 0.5;
+
+nmovies = 6;
+
+nimages = 42;
+idx_end = [56 60 47 44 46 42];
+idx_start = idx_end - nimages + 1;
+
+image_set = zeros(npixels, npixels, nimages, nmovies, 'uint8');
+time_set = zeros(nimages, nmovies);
+
+nrot = 36;
+nshifts = 0;
+shift_max = 0.1;
+
+neigs = 10;
+alpha = 0;
+
+channel = 1;
+
+theta_err = zeros(nmovies, 1);
+rank_corr = zeros(nmovies, 1);
+
+%%
+for k=1:nmovies
+    load(sprintf('movie%d.mat', k));
+    idx = idx_start(k):idx_end(k);
+    
+    images = images(:, :, :, idx);
+    
+    for i=1:nimages
+        image_set(:, :, i, k) = image_fn_shuffledmovie(images(:, :, channel, i), npixels);
+    end
+    
+    time_set(:,k) = dt * (time(idx) - time(idx(1)));
+    
+end
+
+%%
+figure;
+for i=1:nmovies
+    for j=1:nimages/3
+        subplot(nmovies, nimages/3, (i-1)*nimages/3+j)
+        imshow(image_set(:, :, 3*j, i))
+    end
+end
+
+%%
+
+rng(123);
+% movies_to_use = [1 2 5 6];
+movies_to_use = [1 2 3 5 6];
+idx = movies_to_use(randi(length(movies_to_use), nimages, 1));
+% idx = randi(nmovies-1, nimages, 1);
+% idx(idx==4) = nmovies;
+image_set_subsampled = zeros(npixels, npixels, nimages, 'uint8');
+for i=1:nimages
+    image_set_subsampled(:, :, i) = image_set(:, :, i, idx(i));
+end
+
+figure;
+for i=1:nimages
+    subplot(6, 7,i)
+    imshow(image_set_subsampled(:,:,i))
+end
+
+%%
+
+[R, W] = compute_pairwise_alignments(image_set_subsampled, nrot, nshifts, shift_max);
+dim = size(R, 1) / nimages;
+
+%
+W2 = W.^2;
+eps = median(W2(:))/10;
+[R_opt, embed_coord, D2, D] = vdm(R, W2, eps, neigs, alpha);
+
+%%
+image_set_aligned = zeros(npixels, npixels, nimages, 'uint8');
+for i=1:nimages
+    R_tmp = R_opt(dim*(i-1)+1:dim*i, :);
+    theta_opt = atan2d(R_tmp(2,1), R_tmp(1,1));
+    image_set_aligned(:,:,i) = imrotate(image_set_subsampled(:,:,i), -theta_opt, 'crop');
+end
+
+%%
+if corr(time_set(:,1), embed_coord(:,1)) < 0
+    embed_coord(:,1) = -embed_coord(:,1);
+end
+
+[~, I] = sort(embed_coord(:,1));
+figure;
+for i=1:nimages
+    subplot(6, 7,i)
+    imshow(image_set_aligned(:,:,I(i)))
+end
+
+make_fig(4,4);
+plot(compute_ranks(time_set(:,1)), compute_ranks(embed_coord(:,1)),'.')
+xlabel('true rank')
+ylabel('recovered rank')
+set(gca, 'xtick', [0 20 40])
+set(gca, 'ytick', [0 20 40])
+axis([0 50 0 50])
+axis square
+
+
+
