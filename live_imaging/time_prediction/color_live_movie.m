21cd021692149ede7875492bd2a5f47c16f5aa13
Carmeline Dsilva <cdsilva@matthew-remillards-macbook-pro.local>
2014-08-27 00:04:47 -0400

    added code to use cellularization fixed images in color movie

--- live_imaging/time_prediction/color_live_movie.m
+++ live_imaging/time_prediction/color_live_movie.m
@@ -6,53 +6,59 @@ close all
 fixed_image_dir = '../../membrane_pictures/composite_108_noyolk';
 nfixed_images = 108;
 
-movies = {'../bomyi_emb01_gast01.avi';...
-    '../bomyi_emb02_gast02.avi';...
-    '../14_0623/emb01_hisRFP_gastrulation.avi';...
-    '../14_0623/emb02_hisRFP_gastrulation.avi';...
-    '../14_0624/emb01_hisRFP_gastrulation.avi';...
-    '../14_0624/emb02_hisRFP_gastrulation.avi'; ...
-    '../0709_emb02_cell_gast.avi'};
+fixed_image_dir2 = '../../membrane_pictures/large_dataset';
+nfixed_images2 = 90;
 
-movies_angle = [-95;
-    -55;
-    -80;
-    -80;
-    -95;
-    -120;
-    -95];
+% movie_file = '../bomyi_emb01_gast01.avi';
+% movie_angle = -95;
+% movie_start = 20;
+% movie_end = 55;
+% 
+% movie_file = '../bomyi_emb02_gast02.avi';
+% movie_angle = -55;
+% movie_start = 25;
+% movie_end = 60;
+% 
+movie_file = '../14_0623/emb01_hisRFP_gastrulation.avi';
+movie_angle = -80;
+movie_start = 1;
+movie_end = 48;
+% 
+% movie_file = '../14_0623/emb02_hisRFP_gastrulation.avi';
+% movie_angle = -80;
+% movie_start = 10;
+% movie_end = 42;
+% 
+% movie_file = '../14_0624/emb01_hisRFP_gastrulation.avi';
+% movie_angle = -95;
+% movie_start = 15;
+% movie_end = 42;
+% 
+% movie_file = '../14_0624/emb02_hisRFP_gastrulation.avi';
+% movie_angle = -120;
+% movie_start = 5;
+% movie_end = 40;
+% 
+% movie_file = '../0709_emb02_cell_gast.avi';
+% movie_angle = -95;
+% movie_start = 110;
+% movie_end = 140;
 
-movies_start = [20 25 15 10 15 5 110];
-movies_end = [55 60 48 42 42 40 140];
 dt = 0.5;
-movie_idx = 3;
 
 npixels = 500;
 channel = 1;
 nchannels = 3;
 
-fixed_images = zeros(npixels, npixels, nchannels, nfixed_images, 'uint8');
-fixed_image_angle = 0;
 
-for i=1:nfixed_images
-    im_tmp = imread(sprintf('%s/ordered%02d.tif', fixed_image_dir, i));
-    
-    fixed_images(:, :, :, i) = imresize(im_tmp, [npixels npixels]);
-end
-i = setdiff(1:nfixed_images, [79 92 94 96 99 100 102:108]);
-
-fixed_images = fixed_images(:,:,:,i);
-nfixed_images = length(i);
-
-[movie_images, movie_times] = read_video(movies{movie_idx}, npixels);
-movie_images = movie_images(:,:,:,movies_start(movie_idx):movies_end(movie_idx));
-movie_times = dt * movie_times(movies_start(movie_idx):movies_end(movie_idx));
+%%
+[movie_images, movie_times] = read_video(movie_file, npixels);
+movie_images = movie_images(:,:,:,movie_start:movie_end);
+movie_times = dt * movie_times(movie_start:movie_end);
 nmovie_images = length(movie_times);
 
-movie_times = movie_times - 8;
-
 for i=1:nmovie_images
-    movie_images(:,:,:,i) = imrotate(movie_images(:,:,:,i), movies_angle(movie_idx), 'crop');
+    movie_images(:,:,:,i) = imrotate(movie_images(:,:,:,i), movie_angle, 'crop');
 end
 
 %%
@@ -62,20 +68,66 @@ for i=1:nmovie_images
     movie_times_est(i) = predict_time_image(movie_images(:,:,channel, i));
 end
 
+time_est_offset = 10;
+movie_times_est = movie_times_est + time_est_offset;
+
 figure;
 plot(movie_times, movie_times_est,'.')
-hold on 
+hold on
 plot([0 10], [0 10], '-r')
 
-%% predict times
+return
 
-pred_times = zeros(nfixed_images, 1);
-pred_times_min = zeros(nfixed_images, 1);
-pred_times_max = zeros(nfixed_images, 1);
+%%
+fixed_images = [];
+pred_times = [];
+
+%%
+fixed_image_angle = 0;
+
+fixed_images_tmp = zeros(npixels, npixels, nchannels, nfixed_images, 'uint8');
+for i=1:nfixed_images
+    im_tmp = imread(sprintf('%s/ordered%02d.tif', fixed_image_dir, i));
+    
+    fixed_images_tmp(:, :, :, i) = imresize(im_tmp, [npixels npixels]);
+end
+i = setdiff(1:nfixed_images, [79 92 94 96 99 100 102:108]);
+
+fixed_images_tmp = fixed_images_tmp(:,:,:,i);
+nfixed_images = length(i);
+
+fixed_images = cat(4, fixed_images, fixed_images_tmp);
+
+% predict times
+
+pred_times_tmp = zeros(nfixed_images, 1);
 
 for i=1:nfixed_images
-    [pred_times(i), pred_times_min(i), pred_times_max(i)] = predict_time_image(fixed_images(:,:,channel, i));
+    [pred_times_tmp(i), ~, ~] = predict_time_image(fixed_images_tmp(:,:,channel, i));
 end
+
+pred_times = [pred_times; pred_times_tmp];
+
+%%
+
+fixed_images_tmp = zeros(npixels, npixels, nchannels, nfixed_images2, 'uint8');
+for i=1:nfixed_images
+    im_tmp = imread(sprintf('%s/lat%02d.tif', fixed_image_dir2, i));
+    
+    fixed_images_tmp(:, :, :, i) = imresize(im_tmp, [npixels npixels]);
+end
+
+fixed_images = cat(4, fixed_images, fixed_images_tmp);
+
+% predict times
+
+pred_times_tmp = zeros(nfixed_images, 1);
+
+for i=1:nfixed_images
+    [pred_times_tmp(i), ~, ~] = predict_time_image(fixed_images_tmp(:,:,channel, i));
+end
+
+pred_times = [pred_times; pred_times_tmp];
 
 %% plot
 figure;
@@ -120,11 +172,11 @@ clf
 
 for i=1:nmovie_images
     imshow(make_gray_nuclei(movie_images_colored(:,:,:,i)))
-
+    
     frame = getframe;
     writeVideo(writerObj,frame);
     clf
 end
 
 close(writerObj);
-    
+
